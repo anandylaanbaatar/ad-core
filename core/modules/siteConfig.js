c2a7b2434924c3d2ab98c7e1fda100c5a7e01957
Anand Davaasuren <anandylaanbaatar@yahoo.com>
2025-06-18 20:12:26 -0700

    Remote integrations config set using modules.

--- core/modules/siteConfig.js
+++ core/modules/siteConfig.js
@@ -10,46 +10,72 @@ export default async function siteConfigModule(moduleOptions, nuxt) {
   })
   let config = siteConfigData.default
 
-  // Commerce
-  if (config.theme.type === "commerce") {
-    // Store Id
-    if (config.storeId) {
-      const credentials = {
-        projectId: process.env.NUXT_FIREBASE_PROJECT_ID,
-        clientEmail: process.env.NUXT_FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.NUXT_FIREBASE_PRIVATE_KEY.replace(
-          /\\n/g,
-          "\n"
-        ).replace(/\\/g, ""),
-      }
-      const app = getApps().length
-        ? getApp()
-        : initializeApp({
-            credential: cert(credentials),
-            databaseURL: process.env.NUXT_FIREBASE_DATABASE_URL,
-          })
-      const db = getDatabase(app)
-
-      console.log(
-        "✅ Site config commerce loaded!",
-        process.env.NUXT_SHOPIFY_STORE_DOMAIN,
-        config.defaults.siteUrl
-      )
-
-      const tenantId = config.features.multitenancy.parentId
-      const storeId = config.storeId
-      const configPath = `${tenantId}/stores/${storeId}/integrations`
-      const snapshot = await db.ref(configPath).once("value")
-
-      if (snapshot.exists()) {
-        const integrations = snapshot.val()
-        console.log(`✅ Site integrations!`, integrations)
-      }
-
-      db.goOffline()
-      await app.delete()
-    }
-  } else {
-    console.log("✅ Site config loaded!", config.defaults.siteUrl)
+  const credentials = {
+    projectId: process.env.NUXT_FIREBASE_PROJECT_ID,
+    clientEmail: process.env.NUXT_FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.NUXT_FIREBASE_PRIVATE_KEY.replace(
+      /\\n/g,
+      "\n"
+    ).replace(/\\/g, ""),
   }
+  const app = getApps().length
+    ? getApp()
+    : initializeApp({
+        credential: cert(credentials),
+        databaseURL: process.env.NUXT_FIREBASE_DATABASE_URL,
+      })
+  const db = getDatabase(app)
+
+  // Commerce
+  if (config.theme.type === "commerce" && config.storeId) {
+    const tenantId = config.features.multitenancy.parentId
+    const storeId = config.storeId
+    const configPath = `${tenantId}/stores/${storeId}/integrations`
+    const snapshot = await db.ref(configPath).once("value")
+
+    if (snapshot.exists()) {
+      const integrations = snapshot.val()
+
+      if (integrations) {
+        // Shopify
+        if (integrations.shopify) {
+          const version = "2025-04"
+          const storeDomain = `${integrations.shopify.store_domain}.myshopify.com`
+
+          process.env.NUXT_SHOPIFY_API_VERSION = version
+          process.env.NUXT_SHOPIFY_STORE_DOMAIN = storeDomain
+          process.env.NUXT_SHOPIFY_GRAPH_ADMIN_ACCESS_TOKEN =
+            integrations.shopify.graph_admin_access_token
+          process.env.NUXT_SHOPIFY_STOREFRONT_ACCESS_TOKEN =
+            integrations.shopify.storefront_access_token
+
+          nuxt.options.runtimeConfig.public.integrations.shopify = true
+          nuxt.options.runtimeConfig.public.features.shopify = {
+            apiVersion: version,
+            domain: storeDomain,
+          }
+        }
+        // Prismic
+        if (integrations.prismic) {
+          nuxt.options.runtimeConfig.public.integrations.prismic = true
+          nuxt.options.runtimeConfig.public.features.prismic =
+            integrations.prismic.repo
+        }
+
+        console.log(`✅ [Commerce] Integrations Set!`)
+      }
+    }
+
+    console.log(
+      "✅ [Commerce] Config Set!",
+      process.env.NUXT_SHOPIFY_STORE_DOMAIN,
+      config.defaults.siteUrl
+    )
+  } else {
+    console.log("✅ [Site] Config Set!", config.defaults.siteUrl)
+  }
+
+  // Turn off db and cleanup app to avoid long running process.
+  db.goOffline()
+  await app.delete()
 }
