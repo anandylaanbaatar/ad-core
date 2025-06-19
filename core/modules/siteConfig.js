e36c38566052c116a8f5f803bff810d0ed0fa0bb
Anand Davaasuren <anandylaanbaatar@yahoo.com>
2025-06-18 19:57:05 -0700

    Updates

--- core/modules/siteConfig.js
+++ core/modules/siteConfig.js
@@ -1,6 +1,10 @@
 import path from "node:path"
 
 export default async function siteConfigModule(moduleOptions, nuxt) {
+  const { initializeApp, cert, getApps, getApp } = await import(
+    "firebase-admin/app"
+  )
+  const { getDatabase } = await import("firebase-admin/database")
   const siteConfigData = await import(path.resolve("config/site.config.json"), {
     with: { type: "json" },
   })
@@ -8,11 +12,40 @@ export default async function siteConfigModule(moduleOptions, nuxt) {
 
   // Commerce
   if (config.theme.type === "commerce") {
-    console.log(
-      "✅ Site config commerce loaded!",
-      process.env.NUXT_SHOPIFY_STORE_DOMAIN,
-      config.defaults.siteUrl
-    )
+    // Store Id
+    if (config.storeId) {
+      const credentials = {
+        projectId: process.env.NUXT_FIREBASE_PROJECT_ID,
+        clientEmail: process.env.NUXT_FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.NUXT_FIREBASE_PRIVATE_KEY.replace(
+          /\\n/g,
+          "\n"
+        ).replace(/\\/g, ""),
+      }
+      const app = getApps().length
+        ? getApp()
+        : initializeApp({
+            credential: cert(credentials),
+            databaseURL: process.env.NUXT_FIREBASE_DATABASE_URL,
+          })
+      const db = getDatabase(app)
+
+      console.log(
+        "✅ Site config commerce loaded!",
+        process.env.NUXT_SHOPIFY_STORE_DOMAIN,
+        config.defaults.siteUrl
+      )
+
+      const tenantId = config.features.multitenancy.parentId
+      const storeId = config.storeId
+      const configPath = `${tenantId}/stores/${storeId}/integrations`
+      const snapshot = await db.ref(configPath).once("value")
+
+      if (snapshot.exists()) {
+        const integrations = snapshot.val()
+        console.log(`✅ Site integrations!`, integrations)
+      }
+    }
   } else {
     console.log("✅ Site config loaded!", config.defaults.siteUrl)
   }
