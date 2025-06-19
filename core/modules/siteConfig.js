40c4d498fd2cf9477cb8488fc715ff64b8bbff54
Anand Davaasuren <anandylaanbaatar@yahoo.com>
2025-06-18 19:46:33 -0700

    Updates

--- core/modules/siteConfig.js
+++ core/modules/siteConfig.js
@@ -1,3 +1,19 @@
+import path from "node:path"
+
 export default async function siteConfigModule(moduleOptions, nuxt) {
-  console.log("✅ Site config loaded!", process.env.NUXT_SHOPIFY_STORE_DOMAIN)
+  const siteConfigData = await import(path.resolve("config/site.config.json"), {
+    with: { type: "json" },
+  })
+  let config = siteConfigData.default
+
+  // Commerce
+  if (config.theme.type === "commerce") {
+    console.log(
+      "✅ Site config commerce loaded!",
+      process.env.NUXT_SHOPIFY_STORE_DOMAIN,
+      config.defaults.siteUrl
+    )
+  } else {
+    console.log("✅ Site config loaded!", config.defaults.siteUrl)
+  }
 }
