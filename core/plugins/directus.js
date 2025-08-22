cf6f4c8fe6bd50c3cc2e533573b3484062da6c47
Anand Davaasuren <anandylaanbaatar@yahoo.com>
2025-08-22 03:06:53 -0700

    Updates

--- core/plugins/directus.js
+++ core/plugins/directus.js
@@ -335,7 +335,8 @@ export default defineNuxtPlugin((nuxtApp) => {
       params: {
         ...params,
         fields: `
-          *
+          *,
+          address.*
         `,
       },
     })
